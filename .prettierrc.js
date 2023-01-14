6914e33d8920bb62eaaea0c3b943841ec1db2c03
Justin Williams <justinwilliams42@gmail.com>
2023-01-14 10:40:00 -0800

    update dotfiles

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+}
